71b6889b5d116dbacd27ec85a0e794d267325ada
Michael Stenta <mike@mstenta.net>
2022-06-17 08:09:04 -0400

    Issue #3285883: Make soil map layer visibility configurable

--- js/farmOS.map.behaviors.nrcs_soil_survey.js
+++ js/farmOS.map.behaviors.nrcs_soil_survey.js
@@ -1,6 +1,12 @@
 (function () {
   farmOS.map.behaviors.nrcs_soil_survey = {
     attach: function (instance) {
+      // Set initial visibility based on settings (defaults to false).
+      var visible = false;
+      var settings = instance.farmMapSettings;
+      if (settings.behaviors && settings.behaviors.nrcs_soil_survey && settings.behaviors.nrcs_soil_survey.visible) {
+        visible = settings.behaviors.nrcs_soil_survey.visible;
+      }
       var opts = {
         title: 'NRCS Soil Survey',
         url: 'https://sdmdataaccess.nrcs.usda.gov/Spatial/SDM.wms',
@@ -8,7 +14,7 @@
           LAYERS: 'MapunitPoly',
           VERSION: '1.1.1',
         },
-        visible: false,
+        visible: visible,
       };
       instance.addLayer('wms', opts);
     }
